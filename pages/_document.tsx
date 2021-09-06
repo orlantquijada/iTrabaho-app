aeb404f2c8edb369236bae36781cd6fac6f4c45e
Orlan Quijada <orlantquijada@gmail.com>
2021-09-06 16:56:55 +0800

    more initial config stuff

---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import type { DocumentContext } from 'next/document'
+import { CssBaseline } from '@geist-ui/react'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = CssBaseline.flush()
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {styles}
+        </>
+      ),
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import { Text, Page, AutoComplete } from '@geist-ui/react'
+
 export default function Home() {
-  return <div>Hello World</div>
+  const options = [
+    { label: 'London', value: 'london' },
+    { label: 'Sydney', value: 'sydney' },
+    { label: 'Shanghai', value: 'shanghai' },
+  ]
+
+  return (
+    <Page>
+      <Text h1>Hello World</Text>
+      <AutoComplete options={options} />
+    </Page>
+  )
 }

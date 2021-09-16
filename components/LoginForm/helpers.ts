48160cf8fa55fd278bac7a0a1f928e40cf34d822
citReyJoshua <reyjoshua.macarat@cit.edu>
2021-09-16 16:45:46 +0800

    ✨finished login ui

---
+++ components/LoginForm/helpers.ts
@@ -0,0 +1,10 @@
+// types
+export interface FormFields {
+  phoneNumber: string
+  password: string
+}
+
+// methods
+
+// constants
+export const required = { required: 'Please fill out this field' }

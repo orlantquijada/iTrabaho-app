5f719ccd36f21bcd0e2af8e28c94ce304dbd9313
Orlan Quijada <orlantquijada@gmail.com>
2021-09-29 18:04:14 +0800

    ssr support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from '@/components/Layout'
 import { globalStyles } from '@/stitches.config'
 import { UserProvider } from '@/utils/hooks/useUser'
+import { IdProvider } from '@radix-ui/react-id'
 import type { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -8,9 +9,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <UserProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <IdProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </IdProvider>
     </UserProvider>
   )
 }

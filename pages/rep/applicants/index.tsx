7cbd8668f781b9cdaef34f3704ecea4b6b755e23
Orlan Quijada <orlantquijada@gmail.com>
2021-10-15 00:59:33 +0800

    feat: connected applicants list to api

--- pages/rep/applicants/index.tsx
+++ pages/rep/applicants/index.tsx
@@ -1,110 +1,35 @@
 import { Container, Grid, Link } from '@/components'
 import ApplicantCardCompact from '@/components/ApplicantCardCompact'
-import { Applicant } from '@/utils/types'
+import { useApplicants } from '@/utils/hooks/useApplicants'
+import useUser from '@/utils/hooks/useUser'
+import { Loading } from '@geist-ui/react'
 
 export default function ApplicantsList() {
+  const user = useUser()
+  const { applicants, isLoading } = useApplicants({
+    params: user?.id ? { LGURepresentative: user.id } : undefined,
+  })
+
   return (
     <Container css={{ py: '$6' }}>
-      <Grid
-        gap="2"
-        css={{ gridTemplateColumns: 'repeat(auto-fill, minmax(250px,1fr))' }}
-      >
-        {/* users list */}
-        {recruitsList.map((recruit, index) => (
-          <Link
-            href="/rep/applicants/1"
-            css={{ textDecoration: 'none' }}
-            key={index}
-          >
-            <ApplicantCardCompact {...recruit} />
-          </Link>
-        ))}
-      </Grid>
+      {!isLoading && applicants ? (
+        <Grid
+          gap="2"
+          css={{ gridTemplateColumns: 'repeat(auto-fill, minmax(250px,1fr))' }}
+        >
+          {applicants.map((recruit) => (
+            <Link
+              href="/rep/applicants/1"
+              css={{ textDecoration: 'none' }}
+              key={recruit.id}
+            >
+              <ApplicantCardCompact {...recruit} />
+            </Link>
+          ))}
+        </Grid>
+      ) : (
+        <Loading />
+      )}
     </Container>
   )
 }
-
-const recruit: Applicant = {
-  id: 1,
-  firstName: 'John',
-  lastName: 'Doe',
-  phoneNumber: '+639222833416',
-  address: '25 Bayabas Ext., Punta Princes, Cebu City, Cebu',
-  fullName: 'John Doe',
-  birthdate: new Date(),
-  userType: 'A',
-  rep: {
-    id: 1,
-    barangay: 'Punta Princesa',
-    birthdate: new Date(),
-    city: 'Cebu City',
-    province: 'Cebu',
-    firstName: 'Jane',
-    lastName: 'Doe',
-    fullName: 'Jane Doe',
-    phoneNumber: '09222833416',
-    userType: 'L',
-  },
-  profile: {
-    yearsOfExperience: 4,
-    highestEducationAttained: 'Primary School',
-    experience: [
-      {
-        end_month: 'January',
-        end_year: 2021,
-        start_year: 2020,
-        start_month: 'October',
-        location: 'Punta Princesa Cebu City',
-        role: 'Software Engineer',
-        company: 'David Dobrik LLC',
-        experienceDetails: [
-          {
-            description:
-              'Write modern, performant, and robust code for a diverse array of client and internal projects',
-          },
-          {
-            description:
-              'Work with a variety of different languages, frameworks, and content management systems such as JavaScript, TypeScript, React, Vue, NativeScript, Node.js, Craft, Prismic, etc.',
-          },
-          {
-            description:
-              'Communicate and collaborate with multi-disciplinary teams of engineers, designers, producers, clients, and stakeholders on a daily basis',
-          },
-          {
-            description:
-              'Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious venture originating from Northeastern',
-          },
-        ],
-      },
-      {
-        end_month: 'January',
-        end_year: 2021,
-        start_year: 2020,
-        start_month: 'October',
-        location: 'Punta Princesa Cebu City',
-        role: 'Software Engineer',
-        company: 'David Dobrik LLC',
-        experienceDetails: [
-          {
-            description:
-              'Write modern, performant, and robust code for a diverse array of client and internal projects',
-          },
-          {
-            description:
-              'Work with a variety of different languages, frameworks, and content management systems such as JavaScript, TypeScript, React, Vue, NativeScript, Node.js, Craft, Prismic, etc.',
-          },
-          {
-            description:
-              'Communicate and collaborate with multi-disciplinary teams of engineers, designers, producers, clients, and stakeholders on a daily basis',
-          },
-          {
-            description:
-              'Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious venture originating from Northeastern',
-          },
-        ],
-      },
-    ],
-  },
-}
-
-const recruitsList = [recruit, recruit, recruit, recruit, recruit, recruit]
